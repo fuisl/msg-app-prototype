61c9d91958ac715a0b4c32e305fce7d47dca6d2b
nhientruong04 <truonghaonhien2822004@gmail.com>
2025-03-11 16:16:47 +0700

    feat: added chat_group table and change VARCHAR UUID to binary. PK of account table renamed to user_id

--- database/init.sql
+++ database/init.sql
@@ -4,10 +4,18 @@ CREATE DATABASE chatdb;
 USE chatdb;
 
 CREATE TABLE user_account (
-    id VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    user_id VARCHAR(36) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
     username VARCHAR(30) UNIQUE NOT NULL,
     user_password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE chat_group (
+    group_id VARCHAR(36) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+    user_id VARCHAR(36) NOT NULL,
+    group_name VARCHAR(50) DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_userid FOREIGN KEY (user_id) REFERENCES user_account(user_id)
 );
 
 INSERT INTO user_account (username, user_password)
@@ -33,3 +41,19 @@ VALUES
 ('satya_nadella', 'shadow'),
 ('linus_torvalds', '123abc'),
 ('richard_stallman', 'hunter2');
+
+-- Create Group 1 (Tech Enthusiasts) with John Doe & Elon Musk
+INSERT INTO chat_group (user_id, group_name) VALUES
+((SELECT user_id FROM user_account WHERE username = 'john_doe'), 'Tech Enthusiasts'),
+((SELECT user_id FROM user_account WHERE username = 'elon_musk'), 'Tech Enthusiasts');
+
+-- Create Group 2 (Movie Lovers) with Scarlett Johansson & Robert Downey
+INSERT INTO chat_group (user_id, group_name) VALUES
+((SELECT user_id FROM user_account WHERE username = 'scarlett_johansson'), 'Movie Lovers'),
+((SELECT user_id FROM user_account WHERE username = 'robert_downey'), 'Movie Lovers');
+
+-- Create Group 3 (Billionaire Club) with Bill Gates, Jeff Bezos, and Elon Musk
+INSERT INTO chat_group (user_id, group_name) VALUES
+((SELECT user_id FROM user_account WHERE username = 'bill_gates'), 'Billionaire Club'),
+((SELECT user_id FROM user_account WHERE username = 'jeff_bezos'), 'Billionaire Club'),
+((SELECT user_id FROM user_account WHERE username = 'elon_musk'), 'Billionaire Club');
